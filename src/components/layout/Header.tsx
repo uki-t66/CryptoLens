2dc4429227e40569ba39f6ddb12470443d0b5fb1
uki-t66 <trupp55nuu@gmail.com>
2024-11-16 01:15:30 +0900

    add asset-screen header

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = ({ headerTitle }:{ headerTitle: string }) => {
+    return (
+        <div className="mb-8 ">
+             <h1 className="text-2xl font-bold text-white bg-gray-800 p-6 rounded-lg shadow-sm border border-gray-700">{headerTitle }</h1>
+        </div>
+    )
+}
---
+++ src/pages/Asset.tsx
@@ -0,0 +1,10 @@
+import { Header } from "../components/layout/Header"
+
+
+export const Asset = () => {
+    return(
+        <>
+            <Header headerTitle="Asset-Management"/>        
+        </>
+    )
+}
