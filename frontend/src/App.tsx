29e16b36791071ea7b07c059889c91d7eeb91ee1
uki-t66 <trupp55nuu@gmail.com>
2024-11-25 17:39:45 +0900

    add ability to open/close Sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,15 +4,28 @@ import { Sidebar } from './components/layout/Sidebar';
 import { Dashboard } from './pages/Dashboard';
 import { Asset } from './pages/Asset';
 import { Transaction } from './pages/Transaction';
+import { useState } from 'react';
 
 
 const App = () => {
+
+  // Sidebarの開閉状態を管理
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  // Sidebarのbuttonクリック時に作動する関数
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+
   return (
     <BrowserRouter>
-      <div className="fixed flex w-full h-full">
+      <div className="flex w-full h-full">
         {/* Sidebarコンテナ */}
-        <nav>
-          <Sidebar />
+        <nav className={`flex-grow bg-gray-800  border border-gray-700 transition-all duration-350 ${isSidebarOpen ? 'w-64' : 'w-16'}`}>
+          <Sidebar 
+            isOpen={isSidebarOpen} // 現在のSidebar開閉状態
+            onToggle={toggleSidebar} // 状態を更新する関数
+          />
         </nav>
         {/* mainコンテナ */}
         <main className="flex-grow p-10">
