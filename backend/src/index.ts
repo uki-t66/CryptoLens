31224ef0d11eff670bc033a745baa5105a297f00
uki-t66 <trupp55nuu@gmail.com>
2024-12-19 23:21:08 +0900

    change to file as backend entry point

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import transactionRoutes from './routes/tx-routes';
+import authRoutes from './routes/authRoutes';
 
 dotenv.config();
 
@@ -12,8 +12,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/transactions', transactionRoutes);
+app.use('/api/auth', authRoutes);
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
