b001e6df00bae711bde3be11a4ad235dc897bbc5
uki-t66 <trupp55nuu@gmail.com>
2024-12-20 17:29:24 +0900

    Set specific cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,14 +2,22 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,  // フロントエンドのURL
+    credentials: true,  // Cookie送受信の許可
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
+  
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/auth', authRoutes);
