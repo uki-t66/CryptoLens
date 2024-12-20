4d6d95d7acbc9b485d69edd8b8c6307fa2b8b191
uki-t66 <trupp55nuu@gmail.com>
2024-12-20 17:39:49 +0900

    add router /verify

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { register, login } from '../controllers/authController';
+import { register, login, verifyAuth } from '../controllers/authController';
 
 const router = Router();  // Routerのインスタンスを作成
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/verify', verifyAuth);
 
 export default router;
